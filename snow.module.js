9d0e272a02c3091a62403ca26d0ce23001a84601
Giorkesk <thrasivoulasguip@gmail.com>
2022-02-15 16:39:19 +0200

    add more

--- snow.module.js
+++ snow.module.js
@@ -1,6 +1,13 @@
 class GUI{
-    constructor(){
-
+    constructor(json){
+        this.parseJson=function(json){
+            let out=[];
+            for(var child in json){
+                
+            }
+            return(out);
+        }
+        this.children=this.parseJson(json);
     }
     getDomElement(){
         return(document.createElement("img"));
