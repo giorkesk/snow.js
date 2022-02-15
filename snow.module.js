389dfd146c7e9658ffed39a1a639a054e489b551
Giorkesk <thrasivoulasguip@gmail.com>
2022-02-15 19:50:57 +0200

    a lot more

--- snow.module.js
+++ snow.module.js
@@ -2,48 +2,63 @@ class GUI{
     constructor(json={}){
         this.parseJson=function(json){
             let out=[];
-            for(var child in json){
+            for(var i=0;i<json.length;i++){
+                let child=json[i];
                 if(child["add"]=="text"){
-                    out.append(new GUI_Text(child["text"]));
+                    out.push(new GUI_Text(child["text"]));
                 }
                 if(child["add"]=="button"){
-                    out.append(new GUI_Button(child["text"]));
+                    out.push(new GUI_Button(child["text"]));
                 }
                 if(child["add"]=="slider"){
-                    out.append(new GUI_Slider(child["min"],child["max"]));
+                    out.push(new GUI_Slider(child["min"],child["max"]));
                 }
                 if(child["add"]=="input"){
-                    out.append(new GUI_Input(child["text"]));
+                    out.push(new GUI_Input(child["text"]));
                 }
                 if(child["add"]=="container"){
-                    out.append(new GUI_Container(this.parseJson(child["children"])));
+                    out.push(new GUI_Container(this.parseJson(child["children"])));
                 }
             }
             return(out);
+        }
+        this.extrHTML=function(group){
+            let out="";
+            for(let i=0;i<group.children.length;i++){
+                let child=group.children[i];
+                if(child.children!=undefined){
+                    out+=child.innerStart+this.extrHTML(child)+child.innerEnd;
+                }else{
+                    out+=child.inner;
+                    out+="<br>";
+                }
+            }
+            return(out);
+        }
+        this.updateElement=function(){
+            this.elem.innerHTML=this.extrHTML(this);
         }
         this.elem=document.createElement("div");
         this.children=this.parseJson(json);
     }
     getDomElement(){
-        console.log(this);
+        this.updateElement();
         return(this.elem);
     }
 }
 
 class GUI_Text{
     constructor(text=""){
-        this.inner=text;
-        this.useHTML=false;
+        this.inner="<span>"+text+"</span>";
     }
     setText(text=""){
-        this.inner=text;
+        this.inner="<span>"+text+"</span>";
     }
 }
 
 class GUI_Button{
     constructor(text=""){
         this.inner="<button>"+text+"</button>";
-        this.useHTML=true;
     }
     setText(text=""){
         this.inner=text;
@@ -53,19 +68,19 @@ class GUI_Button{
 class GUI_Slider{
     constructor(min=0,max=100){
         this.inner="<input type='range' min='"+String(min)+"' max='"+String(max)+"'/>";
-        this.useHTML=true;
     }
 }
 
 class GUI_Input{
     constructor(text=""){
         this.inner="<input type='text' value='"+text+"'/>";
-        this.useHTML=true;
     }
 }
 class GUI_Container{
     constructor(children=[]){
             this.children=children;
+            this.innerStart="<div>";
+            this.innerEnd="</div>";
     }
 }
 export {GUI,GUI_Text,GUI_Button,GUI_Slider,GUI_Input};
