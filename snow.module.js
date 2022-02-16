4de3eb00e4b2b003a86e0f195447868c6799449b
Giorkesk <thrasivoulasguip@gmail.com>
2022-02-16 19:02:29 +0200

    add some things

--- snow.module.js
+++ snow.module.js
@@ -2,63 +2,56 @@ class GUI{
     constructor(json={}){
         this.parseJson=function(json){
             let out=[];
-            for(var i=0;i<json.length;i++){
-                let child=json[i];
+            for(var child in json){
                 if(child["add"]=="text"){
-                    out.push(new GUI_Text(child["text"]));
+                    out.append(new GUI_Text(child["text"]));
                 }
                 if(child["add"]=="button"){
-                    out.push(new GUI_Button(child["text"]));
+                    out.append(new GUI_Button(child["text"]));
                 }
                 if(child["add"]=="slider"){
-                    out.push(new GUI_Slider(child["min"],child["max"]));
+                    out.append(new GUI_Slider(child["min"],child["max"]));
                 }
                 if(child["add"]=="input"){
-                    out.push(new GUI_Input(child["text"]));
+                    out.append(new GUI_Input(child["text"]));
                 }
                 if(child["add"]=="container"){
-                    out.push(new GUI_Container(this.parseJson(child["children"])));
+                    out.append(new GUI_Container(this.parseJson(child["children"])));
+                }
+                if(child["add"]=="window"){
+                    out.push(new GUI_Window(this.parseJson(child["children"]),child["x"],child["y"],child["flip"]));
                 }
             }
             return(out);
-        }
-        this.extrHTML=function(group){
-            let out="";
-            for(let i=0;i<group.children.length;i++){
-                let child=group.children[i];
-                if(child.children!=undefined){
-                    out+=child.innerStart+this.extrHTML(child)+child.innerEnd;
-                }else{
-                    out+=child.inner;
-                    out+="<br>";
-                }
-            }
-            return(out);
-        }
-        this.updateElement=function(){
-            this.elem.innerHTML=this.extrHTML(this);
         }
         this.elem=document.createElement("div");
         this.children=this.parseJson(json);
     }
     getDomElement(){
-        this.updateElement();
+        console.log(this);
         return(this.elem);
     }
 }
 
 class GUI_Text{
     constructor(text=""){
+<<<<<<< Updated upstream
+        this.inner=text;
+        this.useHTML=false;
+=======
         this.inner="<span>"+text+"</span>";
+        this.id="i"+String(Math.random()*10000);
+>>>>>>> Stashed changes
     }
     setText(text=""){
-        this.inner="<span>"+text+"</span>";
+        this.inner=text;
     }
 }
 
 class GUI_Button{
     constructor(text=""){
         this.inner="<button>"+text+"</button>";
+        this.useHTML=true;
     }
     setText(text=""){
         this.inner=text;
@@ -68,19 +61,33 @@ class GUI_Button{
 class GUI_Slider{
     constructor(min=0,max=100){
         this.inner="<input type='range' min='"+String(min)+"' max='"+String(max)+"'/>";
+        this.useHTML=true;
     }
 }
 
 class GUI_Input{
     constructor(text=""){
         this.inner="<input type='text' value='"+text+"'/>";
+        this.useHTML=true;
     }
 }
+
 class GUI_Container{
     constructor(children=[]){
             this.children=children;
-            this.innerStart="<div>";
-            this.innerEnd="</div>";
     }
 }
-export {GUI,GUI_Text,GUI_Button,GUI_Slider,GUI_Input};
+
+class GUI_Window{
+    constructor(children=[],x=25,y=25,flip=false){
+            this.children=children;
+            if(flip){
+                this.innerStart="<div style='position:fixed;float:right;right:"+String(x)+"px;top:"+String(y)+"px;'>";
+            }else{
+                this.innerStart="<div style='position:fixed;float:left;left:"+String(x)+"px;top:"+String(y)+"px;'>";
+            }
+            this.innerEnd="</div>";
+    }
+
+}
+export {GUI,GUI_Text,GUI_Button,GUI_Slider,GUI_Input,GUI_Window};
